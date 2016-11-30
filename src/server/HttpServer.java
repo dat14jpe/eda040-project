c850b9a9737700d490c3f76387da46150a61b315
Bob Bobson <bobson12@student.lth.se>
2016-11-30 10:17:10 +0100

    HTTP server exception reorganisation

--- src/server/HttpServer.java
+++ src/server/HttpServer.java
@@ -26,50 +26,54 @@ public class HttpServer implements Runnable {
             System.out.println("Camera HTTP server on port " + portNumber);
             Image lastImage = null;
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                OutputStream out = clientSocket.getOutputStream();
-
-                // Read request.
-                String request, inputLine;
-                request = inputLine = in.readLine();
-                while (inputLine != null && !inputLine.equals("")) {
-                    inputLine = in.readLine();
-                }
-
-                // See that it is a GET request.
-                if (null != request && !request.startsWith("GET ")) {
-                    writeln(out, "HTTP/1.0 501 Method not implemented");
-                    writeln(out, "");
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    OutputStream out = clientSocket.getOutputStream();
+    
+                    // Read request.
+                    String request, inputLine;
+                    request = inputLine = in.readLine();
+                    while (inputLine != null && !inputLine.equals("")) {
+                        inputLine = in.readLine();
+                    }
+    
+                    // See that it is a GET request.
+                    if (null != request && !request.startsWith("GET ")) {
+                        writeln(out, "HTTP/1.0 501 Method not implemented");
+                        writeln(out, "");
+                        clientSocket.close();
+                        continue;
+                    }
+    
+                    // Send JPEG image or HTML index (depending on request).
+                    // - Since this is extra functionality, we will default to
+                    // just showing the image directly if there is no index.html.
+                    final String indexPath = "web/index.html";
+                    final boolean indexExists = new File(indexPath).isFile();
+                    writeln(out, "HTTP/1.0 200 OK");
+                    if (!indexExists || request.substring(4, 10).equals("/image")) {
+                        Image image = monitor.getImage(lastImage);
+                        lastImage = image;
+                        byte[] imageData = image.getData();
+                        writeln(out, "Content-Length: " + imageData.length);
+                        writeln(out, "Content-Type: image/jpeg");
+                        writeln(out, "Cache-Control: no-store");
+                        writeln(out, "");
+                        out.write(imageData);
+                    } else {
+                        String content = new String(Files.readAllBytes(Paths.get("web/index.html")));
+                        writeln(out, "Content-Length: " + content.length());
+                        writeln(out, "Content-Type: text/html; charset=utf-8");
+                        writeln(out, "");
+                        writeln(out, content);
+                    }
+    
+                    out.flush();
                     clientSocket.close();
+                } catch (IOException e) {
                     continue;
                 }
-
-                // Send JPEG image or HTML index (depending on request).
-                // - Since this is extra functionality, we will default to
-                // just showing the image directly if there is no index.html.
-                final String indexPath = "web/index.html";
-                final boolean indexExists = new File(indexPath).isFile();
-                writeln(out, "HTTP/1.0 200 OK");
-                if (!indexExists || request.substring(4, 10).equals("/image")) {
-                    Image image = monitor.getImage(lastImage);
-                    lastImage = image;
-                    byte[] imageData = image.getData();
-                    writeln(out, "Content-Length: " + imageData.length);
-                    writeln(out, "Content-Type: image/jpeg");
-                    writeln(out, "Cache-Control: no-store");
-                    writeln(out, "");
-                    out.write(imageData);
-                } else {
-                    String content = new String(Files.readAllBytes(Paths.get("web/index.html")));
-                    writeln(out, "Content-Length: " + content.length());
-                    writeln(out, "Content-Type: text/html; charset=utf-8");
-                    writeln(out, "");
-                    writeln(out, content);
-                }
-
-                out.flush();
-                clientSocket.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
